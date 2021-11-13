0a199ee79daca12127a76973cf7cb300a3c5f4cf
trevin-livele <liveletrevin6@gmail.com>
2021-11-13 12:07:33 +0300

    created days of the week array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 let maleAkaNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 
 let femaleAkanNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yea', 'Afua', 'Ama']
+
+
+let daysofweek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
